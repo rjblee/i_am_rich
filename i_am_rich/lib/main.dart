6e9119fd8e237000f5e947c31d7c460a80d34719
Ralph Lee <ralph.lee@live.ca>
2020-05-01 00:11:18 -0700

    Add appBar, backgroundColor, and image to center

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -5,8 +5,15 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text("I Am Rich"),
+            title: Text('I am Rich'),
             backgroundColor: Colors.blueGrey[900],
+          ),
+          backgroundColor: Colors.blueGrey,
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  "https://postmediacanadadotcom.files.wordpress.com/2019/06/nathan-dumlao-426648-unsplash.jpg"),
+            ),
           ),
         ),
       ),
