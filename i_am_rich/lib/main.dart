7f3dfe2f93b61f9b9526a4f5b61304765091503c
Ralph Lee <ralph.lee@live.ca>
2020-04-30 23:03:35 -0700

    Set up a new Flutter project

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our flutter apps.
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("I Am Rich"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+        ),
+      ),
+    );
